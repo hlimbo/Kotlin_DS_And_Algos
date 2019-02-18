da8b1b8a797faeefeadc57e3506e02fa795d1c2b
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-02-17 21:00:42 -0800

    tried and failed :(

--- src/main/kotlin/dailyProblem48.kt
+++ src/main/kotlin/dailyProblem48.kt
@@ -5,48 +5,59 @@ import kotlin.math.log
 
 class dp48 {
     inner class Node(val letter: Char) {
-        var parent: Node? = null
+        var leftNode: Node? = null
+        var rightNode: Node? = null
     }
 }
 // assumption: preOrderList contains an identical set of characters in inOrderList
 // otherwise the problem will become impossible to solve..
-fun dailyProblem48(preOrderList: ArrayList<Char>, inOrderList: ArrayList<Char>) : dp48.Node {
-
-    // base case: Both arrays have size of 1
-    if(preOrderList.size == 1) {
-        val c: Char = preOrderList[0]
-        println("Base Case: Character used: $c")
-        return dp48().Node(c)
-    }
-
-    preOrderList.removeAt(0)
-    dailyProblem48(preOrderList, inOrderList)
-
-    return dp48().Node('R')
+fun dailyProblem48(preOrderList: ArrayList<Char>, inOrderList: ArrayList<Char>) : dp48.Node? {
+    // in a preOrder traversal the root node will always be the first element in an array representation
+    val preOrderIndex = 0
+    // in an inOrder traversal, the root node will always be the middle element in an array representation
+    val inOrderIndex = inOrderList.size / 2
+    return buildTree(preOrderList, preOrderIndex, inOrderList, inOrderIndex)
 }
 
-//fun helper(preOrderList: ArrayList<Char>): dp48.Node {
-//    val rootChar: Char = preOrderList.removeAt(0)
-//    val root = dp48().Node(rootChar)
-//    return bottomUp(preOrderList, root)
-//}
-//
-//// modify root by implicitly adding in child nodes to it
-//fun bottomUp(preOrderList: ArrayList<Char>, root: dp48.Node?) {
-//
-//    val dp = dp48()
-//    // base case
-//    if(preOrderList.size == 1) {
-//        val n = dp.Node(preOrderList[0])
-//        n.parent = root
-//        return n
-//    }
-//
-//    val c: Char = preOrderList.removeAt(0)
-//    val node = dp.Node(c)
-//    node.parent = root
-//    bottomUp(preOrderList, node)
-//    bottomUp(preOrderList, node)
-//
-//    return dp48().Node('R')
-//}
+// From GeekForGeeks
+/*
+    1. Pick an element from Preorder, Increment a Preorder Index variable
+    to pick next element in next recursive call
+    2. Create a new tree node tNode with the data as picked element
+    3. Find the picked element's index in Inorder. Let the index be inIndex
+    4. Call buildTree for elements before inIndex and make the built tree as right subtree of tNode
+    5. return tNode
+ */
+
+fun buildTree(preOrderList: ArrayList<Char>, preOrderIndex: Int, inOrderList: ArrayList<Char>, inOrderIndex: Int) : dp48.Node? {
+    val dp = dp48()
+
+    // base case ~ return ! character to indicate end of preOrderList has been reached
+    if(preOrderIndex == preOrderList.size)
+        return null
+
+    // 1
+    var pIndex = preOrderIndex
+    pIndex++
+
+    // 2
+    val node = dp.Node(preOrderList[preOrderIndex])
+
+    // 3
+    var iIndex = -1
+    for(i in 0 until inOrderList.size) {
+        if(inOrderList[i] == preOrderList[preOrderIndex]) {
+            iIndex = i
+            break
+        }
+    }
+
+    // 4
+    node.leftNode = buildTree(preOrderList, pIndex, inOrderList, iIndex - 1)
+
+    // 5
+    node.rightNode = buildTree(preOrderList, pIndex, inOrderList, iIndex + 1)
+
+    // 6
+    return node
+}
--- src/test/kotlin/DailyProblem48Test.kt
+++ src/test/kotlin/DailyProblem48Test.kt
@@ -3,10 +3,38 @@ import io.kotlintest.specs.FunSpec
 
 // FAILED TO SOLVE
 class DailyProblem48Test : FunSpec({
-    test("First Test Case") {
-        val dp = dp48()
 
+    fun preOrder(treeNode: dp48.Node?) {
+        if(treeNode == null)
+            return
+
+        println(treeNode.letter)
+        preOrder(treeNode.leftNode)
+        preOrder(treeNode.rightNode)
+    }
+
+    fun inOrder(treeNode: dp48.Node?) {
+        if(treeNode == null)
+            return
+
+        inOrder(treeNode.leftNode)
+        println(treeNode.letter)
+        inOrder(treeNode.rightNode)
+    }
+
+    test("First Test Case") {
         val preOrderList: ArrayList<Char> = arrayListOf('a', 'b', 'd', 'e', 'c' ,'f', 'g')
         val inOrderList: ArrayList<Char> = arrayListOf('d', 'b', 'e', 'a', 'f', 'c' , 'g')
+
+        val treeNode: dp48.Node? = dailyProblem48(preOrderList, inOrderList)
+
+        // to verify print out tree node in preOrder and inOrder orders and see if they are equal
+        // to the original inputs order
+
+        println("preOrder traversal")
+        preOrder(treeNode)
+        println()
+        println("inOrder traversal")
+        inOrder(treeNode)
     }
 })
