ecf7f0881a50234b4247ce208473c2e7c422fb72
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-02-26 01:23:54 -0800

    incomplete daily problem 59

---
+++ src/main/kotlin/dailyProblem59.kt
@@ -0,0 +1,1 @@
+// was a design / networking problem
---
+++ src/test/kotlin/DailyProblem59Test.kt
@@ -0,0 +1,8 @@
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.FunSpec
+
+class DailyProblem59Test : FunSpec({
+    test("First Test Case") {
+    }
+})
