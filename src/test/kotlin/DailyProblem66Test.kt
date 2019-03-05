0880abb7cce64d74724278eb07420fa1cd27fd08
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-03-05 02:22:44 -0800

    incomplete daily problem 66

---
+++ src/test/kotlin/DailyProblem66Test.kt
@@ -0,0 +1,10 @@
+import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.should
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.FunSpec
+
+class DailyProblem66Test : FunSpec({
+    test("First Test Case") {
+    }
+})
